0adf7ec30972dd7e97b26a15af81a4b04e2eab33
Bibechana8 <bibechanapoudel25@gmail.com>
2026-03-06 12:23:57 +0545

    final change

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let userscore = 0;
-let computerscore = 0;
+let userScore = 0;
+let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
 
 const genCompChoice= () => {
     const options = ["rock", "paper", "scissors"];
@@ -11,28 +14,30 @@ const genCompChoice= () => {
 
 };
 const drawGame = () => {
-    console.log("game was draw.");
     msg.innerText = "Game was draw! play again";
     msg.style.backgroundColor = "#081b31";
 };
-const showWinner = (userWin) => {
+const showWinner = (userWin, userChoice, compChoice) => {
     if(userWin){
-        console.log("you win.");
-        msg.innerText = "you win!";
+        userScore++;
+        userScorePara.innerText=userScore;
+        msg.innerText = `you win! Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green";
     }
     else {
-        console.log("you lose.");
-        msg.innerText = "you lose!";
+        compScore++;
+        compScorePara.innerText=compScore;
+        msg.innerText = `you lose! ${compChoice} beats Your ${userChoice}`;
         msg.style.backgroundColor = "red";
     }
 };
 
 const playGame = (userChoice) => {
-    console.log("user choice = ",userChoice);
+    console.log("user choice = ", userChoice);
     // Generate computer choice
     const compChoice = genCompChoice();
-    console.log("comp choice = ",compChoice);
+    console.log("comp choice = ", compChoice);
+
     if(userChoice === compChoice){
         drawGame();
     } else {
@@ -47,7 +52,7 @@ const playGame = (userChoice) => {
             //rock, paper
             userWin = compChoice === "rock" ? false : true;
         }
-        showWinner(userWin);
+        showWinner(userWin, userChoice, compChoice);
     }
 
 };
