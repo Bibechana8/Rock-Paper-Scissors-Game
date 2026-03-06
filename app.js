d7701d3445e28b0954b2633af84e4e963a3896e1
Bibechana8 <bibechanapoudel25@gmail.com>
2026-03-06 10:44:42 +0545

    some logic

--- app.js
+++ app.js
@@ -5,13 +5,44 @@ const choices = document.querySelectorAll(".choice");
 
 const genCompChoice= () => {
     const options = ["rock", "paper", "scissors"];
-    Math.random()
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
 
-}
+};
+const drawGame = () => {
+    console.log("game was draw.");
+};
+const showWinner = (userWin) => {
+    if(userWin){
+        console.log("you win.");
+    }
+    else {
+        console.log("you lose.");
+    }
+};
 
 const playGame = (userChoice) => {
     console.log("user choice = ",userChoice);
     // Generate computer choice
+    const compChoice = genCompChoice();
+    console.log("comp choice = ",compChoice);
+    if(userChoice === compChoice){
+        drawGame();
+    } else {
+        let userWin = true;
+        if(userChoice ==="rock"){
+            //scissors, paper
+            userWin = compChoice === "paper" ? false : true;
+        } else if(userChoice === "paper"){
+            //rock, scissors
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            //rock, paper
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin);
+    }
+
 };
 
 choices.forEach((choice) => {
